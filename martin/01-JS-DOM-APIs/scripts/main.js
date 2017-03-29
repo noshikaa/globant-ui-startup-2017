6cfca4008ee5040286ae95358b04f94cf83808f0
Martin Scocco <martin.scocco@gmail.com>
2017-03-28 22:10:02 -0300

    done until exercise 6 included

--- martin/01-JS-DOM-APIs/scripts/main.js
+++ martin/01-JS-DOM-APIs/scripts/main.js
@@ -1,12 +1,26 @@
-    //this is my function build it to "fadein", but i dont know how to manipulate velocity 
-    //and timers for the face transition, so i will use CSS3 animations.
-    /*function fadeIn(){
-    	let la = 0;
-      while((la += 0.1) < 1){
-      	document.getElementById("fade_in").style.opacity = la;
-      }
-    }*/
+
     function go_to_index(){
       window.location.href = 
       "file:///home/steve/Desktop/bootcamp/globant-ui-startup-2017/martin/01-JS-DOM-APIs/index.html";
     }
+
+    function loadXMLDoc(){
+
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("joke").innerHTML =
+          this.responseText;
+        }
+      };
+      xmlhttp.open("GET", "http://api.icndb.com/jokes/random", true);
+      xmlhttp.send();
+    }
+    //this is my function build it to "fadein", but i dont know how to manipulate velocity 
+    //and timers for the face transition, so i will use CSS3 animations.
+    /*function fadeIn(){
+      let la = 0;
+      while((la += 0.1) < 1){
+        document.getElementById("fade_in").style.opacity = la;
+      }
+    }*/
