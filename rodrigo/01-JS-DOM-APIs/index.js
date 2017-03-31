e98b91d0c8423ac190dbe8ce056c1da9c558b44f
Rodri <rodrigo.zampa@gmail.com>
2017-03-31 00:38:16 -0300

    Finished Topic 1. Added CSS fixes

--- rodrigo/01-JS-DOM-APIs/index.js
+++ rodrigo/01-JS-DOM-APIs/index.js
@@ -1,6 +1,6 @@
 const config = {
   // url: "http://api.icndb.com/jokes/random",
-  url: "https://api.github.com/search/repositories?q=",
+  url: "",
   method: "GET",
   q: ""
 };
@@ -11,6 +11,7 @@ function fadeIn() {
 window.onload = fadeIn;
 
 function responsePromise(config) {
+  config.url="http://api.icndb.com/jokes/random"
   return new Promise(function(resolve, reject) {
     let xhttp = new XMLHttpRequest();
     xhttp.open(config.method, config.url, true);
@@ -35,13 +36,14 @@ function responsePromise(config) {
 }
 
 function responseGit() {
+  config.url = "https://api.github.com/search/repositories?q="
   config.q = document.getElementById("txtSearch").value;
   let xhttp = new XMLHttpRequest();
   xhttp.open(config.method, config.url + config.q, true);
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) { // Typical action to be performed when the document is ready:
       let json = JSON.parse(xhttp.responseText);
-      console.log(json.items[29].full_name);
+      // console.log(json.items[29].full_name);
       console.log(json.items.length);
       //iterate through all json
       // for (let i in json) {
