f50125c36f26ce5e8324d3b8bc7d6717e759b0e1
facusuarez <facusuarez13@gmail.com>
2017-04-01 14:17:05 -0300

    Punto 12 terminado

---
+++ facundo/01-JS-DOM-APIs/exercise12/script.js
@@ -0,0 +1,38 @@
+
+window.addEventListener("load", function () {
+    document.getElementById('tit').style.visibility = "hidden"
+    document.getElementById("sub").addEventListener("click", loadtable);
+}, false);
+
+function loadtable() {
+    document.getElementById('tab').innerHTML="";
+    let fil = document.getElementById("fil").value
+    let col = document.getElementById("col").value
+    createArray(fil, col)
+}
+
+function createArray(fil, col) {
+    var array = [];
+    for (var i = 0; i < fil; i++) {
+        array[i] = [];
+        for (var j = 0; j < col; j++) {
+            array[i][j] = (Math.random() * 100 | 0);
+        }
+    }
+    createTable(array)
+}
+function createTable(array) {
+    var table = document.getElementById("tab");
+    var tbody = document.createElement("tbody");
+    table.appendChild(tbody);
+    array.forEach(function (items) {
+        var row = document.createElement("tr");
+        items.forEach(function (item) {
+            var cell = document.createElement("td");
+            cell.textContent = item;
+            row.appendChild(cell);
+        });
+        tbody.appendChild(row);
+    });
+    document.getElementById('tit').style.visibility = "visible";
+}
