f7ef55e0768d670f2ced5ee8e5c69ec605d55e52
Macarena Saenz <macarena.saenzvc@gmail.com>
2017-04-01 19:20:55 -0300

    Finished topic 1

--- maca/01-JS-DOM-APIs/assets/js/index.js
+++ maca/01-JS-DOM-APIs/assets/js/index.js
@@ -10,8 +10,10 @@ window.onload = function(){
 			document.getElementById('hidden').style.opacity = 1;
 			document.getElementById("btn").addEventListener("click", getJoke2);
 			document.getElementById("btn2").addEventListener("click", getNames);
-		}
 
+}
+
+//topic 1 - ex. 6
 getJoke = function(){
 		let req = new XMLHttpRequest();
 		req.open('GET', "http://api.icndb.com/jokes/random", true);
@@ -19,12 +21,12 @@ getJoke = function(){
 	        	if(req.readyState === XMLHttpRequest.DONE && req.status === 200) {
 	        		let jsonResp = JSON.parse(req.responseText);
 	        		document.getElementById('hidden').innerHTML = jsonResp.value.joke;
-	        		document.getElementById('hidden').style.background = "red"		            
 	        }
 	    };
 		req.send();
 };
 
+//topic 1 - ex. 7
 promiseFunct = function(config){
 	let prom = new Promise(function(resolve, reject){
 		let req = new XMLHttpRequest();
@@ -56,32 +58,33 @@ getJoke2 = function(){
 		.then(function(response){			
 			let jsonResp = JSON.parse(response);
 			document.getElementById('hidden').innerHTML = jsonResp.value.joke;
-	        document.getElementById('hidden').style.background = "#66ffcc";
+	        document.getElementById('hidden').className = "jokeOK";
 		})
 		.catch(function(error) {
 			console.error("Failed!", error);
 			document.getElementById('hidden').innerHTML = "ERROR!";
-	        document.getElementById('hidden').style.background = "red";
-		})
+	        document.getElementById('hidden').className = "jokeError";		
+		})						
 };
 
+//topic 1 - ex. 9
 getNames = function(){	
 	config.method = "GET";
 	config.q = document.getElementById('q').value;
 	config.url = "https://api.github.com/search/repositories?q="+config.q;
 	promiseFunct(config)
 	.then(function(response){
-		list = document.getElementById('list');
+		list = document.createElement("ul");
 		respJson = JSON.parse(response);
 		for (var obj in respJson.items){
-			let li = document.createElement("li");
+			const li = document.createElement("li");
 			let text = document.createTextNode(respJson.items[obj].full_name);
 			li.appendChild(text);
 			list.appendChild(li);
-        /*    list.innerHTML += "<li>" + respJson.items[obj].full_name + "</li> <br>";*/
             }
-        list.style.borderColor = "#33bbff";
-  		list.style.borderStyle = "dashed"; 
+        const listWrap = document.getElementById('listWrapper').appendChild(list);   
+        listWrap.className = "list";
+        
 	})
 	.catch(function(error) {
 		console.error("Failed!", error);	  				
--- maca/01-JS-DOM-APIs/assets/js/mToT.js
+++ maca/01-JS-DOM-APIs/assets/js/mToT.js
@@ -20,18 +20,20 @@ let createMatrix = function(){
 }
 
 let toTable = function(arr){
-	console.log("entro")
-	//let arr = [[11111,2],[3,4], [5,6]];
-	let table = document.getElementById("table");	
+
+	const wrapper = document.getElementById("tableWrap");		 
+	const table = document.createElement("table");		 	
+	
 	for (var i = 0; i < arr.length; i++) {
-		let row = document.createElement("tr");
+		const row = document.createElement("tr");			
 		for (var j = 0; j < arr[1].length; j++) {
-			let td = document.createElement("td");
+			const td = document.createElement("td");		
 			let data = document.createTextNode(arr[i][j]);
 			td.appendChild(data);
 			row.appendChild(td);
 		}
 		table.appendChild(row);
 	}
+	wrapper.appendChild(table);
 }
 
