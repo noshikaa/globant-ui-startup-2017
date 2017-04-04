b8a48564471743e072fa87e36a4a085c62f34d1b
Martin Scocco <martin.scocco@gmail.com>
2017-04-04 17:07:09 -0300

    exer3 complete from scratch, hasn't been tested

--- martin/02-oop-inheritance/movie.js
+++ martin/02-oop-inheritance/movie.js
@@ -11,39 +11,44 @@ var Movie = {
 //exer 3.
 class EventEmitter {
   constructor() {
-    this.events = {}; //can use _events instead of events to indicate that it should 
+    this.events = {}; //this is an object.
+    //can use '_events' instead of 'events' to indicate that it should 
     //be trated as a private property.
   }
-  this.on(event, callback) { //the 'event' parameter is neccesary? i think yes because
-    //every time the event is called, it will trigger the exact same callback(listener).
-    console.log("an event ocurred");
-    callback && callback(); //if callback exists(the && return TRUE), run it.
+  on(event, callback) {
+    //check if the event do not exists as an object.
+    if (typeof this.events[event] !== 'object') { // 'this.events[event] === undefined' are the same.
+      this.events[event] = []; //this is an empty array;
+      console.log("an event has been pushed"); //this is just to SEE the advance in console.
+    }
+    this.events[event].push(callback); //introduce the callback into the array
   }
-  this.emit(event) {
-
+  emit(event) {
+    if (typeof this.events[event] === 'object') { //check if it is an object.
+      this.events[event].forEach(function(i) {
+        i.apply();
+      })
+    } else console.error('uknown event');
+    //when a method is executed, emmit it. Example: Movie.play(); <= call emit;
   }
-  this.off(event, method) { //we pass the event and the method to remove from.
-    //search every event until match, and delete it. 
-    for ( in event) {
+  off(event, callback) { //we pass the event and the function/method to remove from the events array.
+    if (typeof this.events[event] === 'object') { //check if the event exists.
+      //search every event in the array until match, and remember the key location. 
+      let key = -1;
+      let i = 0;
+      let length = this.events[event].length;
+      let found = false;
 
+      while (i < length && !found) {
+        if (this.events[event][i] === callback) {
+          key = i;
+          found = true;
+        }
+        i++;
+      }
+      //having the location in the array, remove the callback.
+      if (key > -1) this.events[event].splice(key, 1);
+      else console.error("the 'callback' cannot be found");
     }
   }
 }
-
-/*
-class Movie {
-  constructor(title, year, duration) {
-    this.title = title
-    this.year = year
-    this.duration = duration
-  }
-  play() {
-
-  }
-  pause() {
-
-  }
-  resume() {
-
-  }
-}*/
