7c5fe5865ddac69d1892b7c8c09be06d4c269db6
Naushikaa <matthieu.milewski@laposte.net>
2017-04-23 17:31:39 -0300

    Topic 3
    point 3 fixed

--- matthieu/03-MVC-AngularJS/Topic 3 indexedDB point 1+2+3/dragAndDrop.js
+++ matthieu/03-MVC-AngularJS/Topic 3 indexedDB point 1+2+3/dragAndDrop.js
@@ -3,9 +3,9 @@
     evt.stopPropagation();
     evt.preventDefault();
 
-    var files = evt.dataTransfer.files; // FileList object.
+    let files = evt.dataTransfer.files; // FileList object.
     // files is a FileList of File objects. List some properties.
-    var read = new FileReader();
+    let read = new FileReader();
 
     //at the end of the file load, we put the text value in textbox
     read.onloadend = function(){
@@ -13,7 +13,7 @@
     }
 
     //in case we have multiple files
-    for (var i = 0, f; f = files[i]; i++) {
+    for (let i = 0, f; f = files[i]; i++) {
     read.readAsBinaryString(f);
     }
     
@@ -26,6 +26,6 @@
   }
 
   // Setup the dnd listeners.
-  var dropZone = document.getElementById('textBox');
+  let dropZone = document.getElementById('textBox');
   dropZone.addEventListener('dragover', handleDragOver, false);
   dropZone.addEventListener('drop', handleFileSelect, false);
